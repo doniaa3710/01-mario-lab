6410e7054c7221bb2f69387762d22ce3453a798f
Donia Abdelhalim <doniaa3710@hstat.org>
2018-10-03 16:22:14 +0000

    ask for the question

--- pyramid.rb
+++ pyramid.rb
@@ -1,11 +1,16 @@
 #write your code here
 
 def pyramid 
-    block = "# #"
-    block_one= block + "##   ##"
-    block_two= block + block_one + "###          ###"
-    block_three= block + block_one + block_two + "####                  ####"
-    block_four= block + block_one + block_two + block_four +  "####                  ####"
-    
-    
+  puts " "*3 + "#"*1 + " "*2 + "#"*1
+  puts " "*2 + "#"*2 + " "*2 + "#"*2
+  puts " "*1 + "#"*3 + " "*2 + "#"*3
+  puts " "*0 + "#"*4 + " "*2 + "#"*4
 end
+pyramid
+
+
+def height
+"how tall they you like to make the pyramid?"
+end 
+height 
+
