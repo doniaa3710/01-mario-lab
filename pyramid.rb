797d03d3ada4da248682d8e1f74c52ae67fd40a7
Donia Abdelhalim <doniaa3710@hstat.org>
2018-10-03 15:38:26 +0000

    add block

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+def pyramid 
+    block = "# #"
+    block_one= block + "##   ##"
+    block_two= block + block_one + "###          ###"
+    block_three= block + block_one + block_two + "####                  ####"
+    block_four= block + block_one + block_two + block_four +  "####                  ####"
+    
+    
+end
